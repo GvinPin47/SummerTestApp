b06d92da807f2cb0a337cf2444e2a8936d8d68ea
GvinPin47 <aragon_1995@mail.ru>
2017-07-25 23:46:18 +0700

    tart_commit

---
+++ SummerTestApp/Activity/SplashScreen.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace SummerTestApp.Activity
+{
+    [Activity(Label = "SummerTestApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class SplashScreen : Android.App.Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
